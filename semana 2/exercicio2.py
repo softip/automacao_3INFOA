066ee21cef75530b81f2c8dc59e0108f5723532b
Ivan Paulino <softip@gmail.com>
2024-03-05 16:58:17 -0300

    exercício 2

--- semana 2/exercicio2.py
+++ semana 2/exercicio2.py
@@ -5,5 +5,13 @@ e a quantidade de faltas de um aluno. O
 programa deve imprimir reprovado, se:
 A nota for menor que 6 ou se as presencas
 forem menor do que 75 e aprovado 
-caso contrário.
+caso contrário .
 '''
+nota = float(input("Digite a nota do aluno: "))
+presencas = int(input("Digite a quantidade de presença: "))
+
+if nota < 6 or presencas < 75:
+    print("Reprovado")
+else:
+    print("Aprovado")
+
